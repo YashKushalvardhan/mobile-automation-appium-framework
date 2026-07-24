19439ec9d65165b9aef87a6d21e40eecd1dab5b0
Yash Kushalvardhan <yashkushalvardhan6@gmail.com>
2026-07-24 21:56:51 +0100

    Add Docker support for containerized test execution

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -7,17 +7,23 @@ import os
 class DriverFactory:
     @staticmethod
     def get_driver():
-        # Path to APK (relative to project root)
         apk_path = os.path.join(os.getcwd(), "apps", "ApiDemos.apk")
 
         options = UiAutomator2Options()
         options.platform_name = "Android"
         options.automation_name = "UiAutomator2"
-        options.device_name = "emulator-5554"   # adb devices name
+        options.device_name = "emulator-5554"
         options.app = apk_path
         options.new_command_timeout = 300
-        options.no_reset = False   # for every state freshapp
+        options.no_reset = False
 
-        # Appium 2.x default server URL (no /wd/hub needed)
-        driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+        # Read Appium server URL from an environment variable.
+        # Defaults to localhost for when tests run directly on the host machine.
+        # When running inside Docker, this will be overridden to point
+        # to the host machine's Appium server via host.docker.internal.
+        appium_server_url = os.environ.get(
+            "APPIUM_SERVER_URL", "http://127.0.0.1:4723"
+        )
+
+        driver = webdriver.Remote(appium_server_url, options=options)
         return driver

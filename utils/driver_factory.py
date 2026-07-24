fda755d49e2f92d4ec4e8e8081c9f564ad76918f
Yash Kushalvardhan <yashkushalvardhan6@gmail.com>
2026-07-24 22:07:41 +0100

    Add Docker support for containerized test execution" -m "Containerized the test runner (Python + Pytest + Appium client) using Docker. Appium server and emulator remain on host due to Windows KVM limitations; container connects via host.docker.internal networking.

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -7,20 +7,21 @@ import os
 class DriverFactory:
     @staticmethod
     def get_driver():
-        apk_path = os.path.join(os.getcwd(), "apps", "ApiDemos.apk")
-
         options = UiAutomator2Options()
         options.platform_name = "Android"
         options.automation_name = "UiAutomator2"
         options.device_name = "emulator-5554"
-        options.app = apk_path
+
+        # The APK path must be valid from the Appium SERVER's perspective,
+        # not the test runner's. Since our Appium server runs on the
+        # Windows host (not inside Docker), we need the Windows filesystem
+        # path here, even when tests execute from inside a Linux container.
+        default_apk_path = os.path.join(os.getcwd(), "apps", "ApiDemos.apk")
+        options.app = os.environ.get("APK_PATH", default_apk_path)
+
         options.new_command_timeout = 300
         options.no_reset = False
 
-        # Read Appium server URL from an environment variable.
-        # Defaults to localhost for when tests run directly on the host machine.
-        # When running inside Docker, this will be overridden to point
-        # to the host machine's Appium server via host.docker.internal.
         appium_server_url = os.environ.get(
             "APPIUM_SERVER_URL", "http://127.0.0.1:4723"
         )

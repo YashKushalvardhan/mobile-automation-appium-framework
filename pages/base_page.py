36f78865a9857abf7bbeb4396e083ad39919c774
Yash Kushalvardhan <yashkushalvardhan6@gmail.com>
2026-07-25 14:38:12 +0100

    Increased Explicit wait by 10 secs

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 15)  # Explicit wait instead of time.sleep()
+        self.wait = WebDriverWait(driver, 25)  # Explicit wait instead of time.sleep()
 
     def find_element(self, by, value):
         return self.wait.until(EC.presence_of_element_located((by, value)))
